e895447ecb415c3bb2b197059da8f009a0df660d
Esmeralda <esmeraldatorres.2305@gmail.com>
2023-09-25 15:11:43 -0600

    database structure and scripts

---
+++ DB/productsBeers_2.sql
@@ -0,0 +1,24 @@
+
+-- Script para Insertar 20 cervezas artesanales mexicanas  en la tabla 'products'
+INSERT INTO db_beercraft.products (name, description, img_url, abv, origin, price, size, stock)
+VALUES
+    ('La Chingonería Pale Ale', 'Pale Ale de La Chingonería con sabores cítricos y florales.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 5.2, 'México', 4.99, 355, 10),
+    ('Cucapá Obscura', 'Cerveza Obscura de Cucapá con notas a café y caramelo.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 4.5, 'México', 4.25, 355, 60),
+    ('Minerva Stout Imperial', 'Stout Imperial de Minerva con sabor a chocolate y vainilla.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 8.0, 'México', 6.99, 330, 8),
+    ('Calavera Catrina', 'IPA Calavera Catrina con un toque picante y cítrico.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 6.5, 'México', 5.50, 355, 45),
+    ('Border Psycho La Perversa', 'American Porter de Border Psycho con notas a toffee y chocolate.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 6.5, 'México', 6.25, 330, 9),
+    ('Colimita', 'Cerveza Colimita estilo Lager con un toque refrescante.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 4.8, 'México', 3.75, 355, 15),
+    ('Tijuana Morena', 'Brown Ale Tijuana Morena con sabor a nueces y caramelo.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 4.5, 'México', 4.50, 355, 12),
+    ('Tempus IPA', 'IPA Tempus con sabores tropicales y un amargor suave.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 6.2, 'México', 5.99, 330, 13),
+    ('Cerveza Loba', 'Cerveza Loba estilo American Amber Ale con notas a frutas secas.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 5.6, 'México', 4.75, 355, 8),
+    ('Cerveza Fauna Obscura', 'Cerveza Fauna Obscura con matices de chocolate y café.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 6.9, 'México', 6.50, 330, 14),
+    ('Baja Brewing Co. Peyote Pale Ale', 'Pale Ale Baja Brewing Co. con un toque herbal y cítrico.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 5.5, 'México', 4.25, 355, 11),
+    ('La Bru', 'Cerveza La Bru estilo Belgian Dubbel con notas a frutas oscuras.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 7.2, 'México', 6.99, 330, 14),
+    ('Cerveza Cucapá La Migra', 'Cerveza Cucapá estilo Blonde Ale con un toque afrutado.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 4.5, 'México', 4.50, 355, 11),
+    ('Lagunitas IPA', 'IPA Lagunitas con un fuerte aroma a lúpulo y cítricos.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 6.2, 'México', 5.99, 330, 14),
+    ('Sierra Madre Day of the Dead', 'Cerveza Day of the Dead estilo Porter con notas a café y chocolate.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 5.8, 'México', 4.75, 355, 16),
+    ('Cerveza Colima Cocolimón', 'Cerveza Colima Cocolimón con coco y limón.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 4.5, 'México', 3.99, 330, 6),
+    ('Tecate Michelada', 'Tecate con jugo de tomate, limón y especias.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 3.5, 'México', 2.99, 355, 4),
+    ('La Sirena Gose', 'Gose La Sirena con un toque salado y cítrico.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 4.2, 'México', 4.25, 330, 7),
+    ('Cerveza Calavera Pilsner', 'Pilsner Calavera con un sabor suave y refrescante.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview.png', 5.0, 'México', 4.50, 355, 1),
+    ('Cerveza Tijuana Hefeweizen', 'Hefeweizen Tijuana con notas a plátano y clavo.', 'https://i.ibb.co/hgTr72X/triplehog-1200x1200-removebg-preview', 5.0, 'México' , 4.50, 355, 10);
